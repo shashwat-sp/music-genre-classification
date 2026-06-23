ea84c51d34fc6d228a8fe840cbb8b4b0de1107a8
ShashwatBisht <shashwat121105@gmail.com>
2026-06-23 21:10:10 +0530

    Create cnn_model.py
    Implement custom CNN architecture for music genre classification

---
+++ cnn_model.py
@@ -0,0 +1,103 @@
+"""CNN model architecture for music genre classification."""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class CNNModel(nn.Module):
+    
+    def __init__(self, input_channels: int, num_classes: int):
+        """
+        Args:
+            input_channels: Number of input channels (1 for mel-spectrogram)
+            num_classes: Number of genre classes
+        """
+        super(CNNModel, self).__init__()
+        
+        self.conv_block1 = nn.Sequential(
+            nn.Conv2d(input_channels, 32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.MaxPool2d(2),
+            nn.Dropout(0.25)
+        )
+        
+        self.conv_block2 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.MaxPool2d(2),
+            nn.Dropout(0.25)
+        )
+        
+        self.conv_block3 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(128),
+            nn.MaxPool2d(2),
+            nn.Dropout(0.25)
+        )
+        
+        self.conv_block4 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(256),
+            nn.AdaptiveAvgPool2d(1),
+            nn.Dropout(0.5)
+        )
+        
+        self.fc = nn.Linear(256, num_classes)
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Forward pass through the network."""
+        x = self.conv_block1(x)
+        x = self.conv_block2(x)
+        x = self.conv_block3(x)
+        x = self.conv_block4(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+
+
+class SimpleCNN(nn.Module):
+    
+    def __init__(self, input_channels: int, num_classes: int):
+        super(SimpleCNN, self).__init__()
+        
+        self.conv1 = nn.Conv2d(input_channels, 32, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.pool1 = nn.MaxPool2d(2)
+        
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.pool2 = nn.MaxPool2d(2)
+        
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.pool3 = nn.MaxPool2d(2)
+        
+        self.conv4 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.bn4 = nn.BatchNorm2d(256)
+        self.pool4 = nn.MaxPool2d(2)
+        
+        self.dropout = nn.Dropout(0.5)
+        self.fc = nn.Linear(256 * 4 * 4, num_classes)
+    
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = self.pool1(x)
+        
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = self.pool2(x)
+        
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = self.pool3(x)
+        
+        x = F.relu(self.bn4(self.conv4(x)))
+        x = self.pool4(x)
+        
+        x = x.view(x.size(0), -1)
+        x = self.dropout(x)
+        x = self.fc(x)
+        return x
